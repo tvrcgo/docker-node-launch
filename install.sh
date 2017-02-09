8383721153c8a7884459c3931a1279006bd5c9ed
tvrcgo <tvrcgo@gmail.com>
2017-02-09 14:42:51 +0800

    install docker compose

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
