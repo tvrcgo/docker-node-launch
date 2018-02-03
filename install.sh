0f94fdb02a78af225029942f9a8dd14bde374621
tvrcgo <tvrcgo@gmail.com>
2018-02-04 01:55:03 +0800

    update COPY

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-# copy sample app
-mkdir -p /home/app
-cp -rn ./node/sample /home/app/sample
-
 # gitlab ci runner
 mkdir -p /etc/docker/gitlab-runner
 cp -n ./gitlab-ci-runner/config.toml /etc/docker/gitlab-runner/config.toml
